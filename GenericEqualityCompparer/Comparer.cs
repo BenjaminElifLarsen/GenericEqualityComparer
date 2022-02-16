4efd0ecd8578c28256916105e7bd3dd2f9ee4cdd
Beela <bla@senia.dk>
2022-02-16 12:45:56 +0100

    Add project files.

---
+++ GenericEqualityCompparer/Comparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericEqualityCompparer
+{
+    internal class GenericComparer<T, U> : IEqualityComparer<T>
+    {
+        private readonly Func<T, U> _key;
+        public GenericComparer( Func<T, U> key)
+        {
+            _key = key;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return _key(x).Equals(_key(y));
+        }
+
+        public int GetHashCode( T obj)
+        {
+            return _key(obj).GetHashCode();
+        }
+    }
+}
---
+++ GenericEqualityCompparer/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using GenericEqualityCompparer;
+
+var str1 = new TestClass { Test = "Test1" };
+var str2 = new TestClass { Test = "Test2" };
+var str3 = new TestClass { Test = "Test1" };
+var str4 = str2;
+
+var compare = new GenericComparer<TestClass, string>(x => x.Test);
+
+Console.WriteLine("Same value: " + compare.Equals(str1, str3));
+Console.WriteLine("Different value: " + compare.Equals(str1, str2));
+Console.WriteLine("Same reference: " + compare.Equals(str4, str2));
